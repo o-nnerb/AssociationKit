54b63ca0dcd137ed695f961d03b792b259ce9477
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2022-09-14 07:36:03 -0300

    Project init

---
+++ Sources/AssociationKit/AssociationKit.swift
@@ -0,0 +1,6 @@
+public struct AssociationKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AssociationKitTests/AssociationKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AssociationKit
+
+final class AssociationKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AssociationKit().text, "Hello, World!")
+    }
+}
